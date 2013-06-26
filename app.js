0ca59b06d4fe9c446b264d76e3ddef12511e49c3
Alex Cline <alex.cline@gmail.com>
2013-06-26 16:15:00 -0400

    Enable proxy mode in express.

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.methodOverride());
 app.use(errorHandler);
 app.use(express.static(__dirname + '/public'));
 app.use(app.router);
-//app.enable('trust proxy');
+app.enable('trust proxy');
 
 app.get('/[0-9]{12}', function(req, res){
   upctoasin.lookup(req.url, function(err, data){
