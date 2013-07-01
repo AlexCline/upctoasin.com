c0a842ed35cdcfda017c43be296470ea3ae248ca
Alex Cline <alex.cline@gmail.com>
2013-07-01 14:58:25 -0400

    Don't save invalid UPCs to the DB.

--- app.js
+++ app.js
@@ -18,8 +18,10 @@ app.get('/[0-9]{12}[0-9]?', function(req, res){
     // If it's not in the db, query amazon
     if(!result) {
       amazon.lookup(upc, function(err, result){
-        // Save the upc and asin to the db
-        sqlite.cache(upc, result, function(err, result){});
+        // Save the upc and asin to the db if it's valid.
+        if (upc != 'UPCNOTFOUND' || upc != 'UPCLOOKUPERROR')
+          sqlite.cache(upc, result, function(err, result){});
+        
         res.send(result);
       });
     } else {
