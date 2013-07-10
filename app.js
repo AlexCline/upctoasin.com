92b66c9782a3bc66ee35561e5d3efff9806a8ca1
Alex Cline <alex.cline@gmail.com>
2013-07-10 00:22:33 -0400

    Add Google Analytics tracking for node.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-var amazon  = require('./lib/amazon.js'),
-    express = require('express'),
-    path    = require('path'),
-    sqlite  = require('./lib/sqlite.js');
+var amazon      = require('./lib/amazon.js'),
+    express     = require('express'),
+    fs          = require('fs'),
+    nodealytics = require('nodealytics'),
+    path        = require('path'),
+    sqlite      = require('./lib/sqlite.js'),
+    yaml        = require('js-yaml');
 
 var app = express();
 app.use(express.bodyParser());
@@ -11,7 +14,16 @@ app.use(express.static(__dirname + '/public'));
 app.use(app.router);
 app.enable('trust proxy');
 
+var google = yaml.load(fs.readFileSync(path.join(__dirname, 'conf/google.yaml'), 'utf8'));
+
 app.get('/[0-9]{12}[0-9]?', function(req, res){
+  nodealytics.initialize(google.id, google.domain, function () {
+    nodealytics.trackPage('UPC Lookup', req.url, function (err, resp) {
+      if (err || resp.statusCode != 200) {
+        console.log('Error tracking with Google Analytics: ' + err);
+      }
+    });
+  });
   // Check if the UPC is in the db
   upc = req.url.slice(1);
   sqlite.lookup(upc, function(err, result){
